62a31b012c437f3ab93327df655f3c1cb8acd3c0
LEE SEOK HWAN <shclub@gmail.com>
2022-06-15 17:25:26 +0900

    first commit

--- src/main/java/com/kt/edu/thirdproject/service/ArticleCommandService.java
+++ src/main/java/com/kt/edu/thirdproject/service/ArticleCommandService.java
@@ -5,21 +5,35 @@ import com.kt.edu.thirdproject.exception.ResourceNotFoundException;
 import com.kt.edu.thirdproject.repository.ArticleCommandRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @Service
 @Transactional
 @Slf4j
 public class ArticleCommandService {
 
-    @Autowired
+    //@Autowired
     private ArticleCommandRepository articleCommandRepository;
 
+    private RestTemplate restTemplate;
+
+    @Autowired
+    public ArticleCommandService(RestTemplateBuilder restTemplateBuilder, ArticleCommandRepository articleCommandRepository){
+        this.articleCommandRepository = articleCommandRepository;
+        this.restTemplate = restTemplateBuilder
+                .setConnectTimeout(Duration.ofSeconds(3))
+                .setConnectTimeout(Duration.ofSeconds(30))
+                .build();
+    }
     /*public ResponseEntity<ArticleEntity> create(ArticleEntity articleEntity) {
         log.info("Request to create ArticleEntity : " +  articleEntity);
         articleEntity.setId(articleCommandRepository.retvNextVal());
@@ -31,8 +45,7 @@ public class ArticleCommandService {
         log.info("Request to create ArticleEntity : " +  articleEntity);
         articleEntity.setId(articleCommandRepository.retvNextVal());
         articleEntity.setNew(true);
-        ArticleEntity createdEntity = articleCommandRepository.save(articleEntity);
-        return createdEntity;
+        return this.articleCommandRepository.save(articleEntity);
     }
 
     /*public ResponseEntity<ArticleEntity> update(ArticleEntity articleEntity) {
@@ -44,8 +57,8 @@ public class ArticleCommandService {
     public ArticleEntity update(ArticleEntity articleEntity) {
         log.info("Request to update ArticleEntity : " +  articleEntity);
         articleEntity.setNew(false);
-        ArticleEntity updatedEntity = articleCommandRepository.save(articleEntity);
-        return updatedEntity;
+        //ArticleEntity updatedEntity = articleCommandRepository.save(articleEntity);
+        return this.articleCommandRepository.save(articleEntity);
     }
     /*public ResponseEntity<ArticleEntity> delete(Long id) {
         log.info("Request to delete ArticleEntity id : " +  id);
